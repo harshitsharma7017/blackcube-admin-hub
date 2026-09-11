0d3fc9374ae4f365f8149e71dbedcfaf0542b8a4
Harshit Sharma <harshit.sharma8532@gmail.com>
2026-09-11 11:22:54 +0530

    Remove .vercel from git to force Vercel to build

--- .vercel/output/static/assets/skeleton-C389QGV-.js
+++
@@ -1,1 +0,0 @@
-import{F as e}from"./queries-BfrRjA_G.js";import{a as t}from"./dist-DLrMyBK-.js";var n=e();function r({className:e,...r}){return(0,n.jsx)(`div`,{className:t(`animate-pulse rounded-md bg-primary/10`,e),...r})}export{r as t};
